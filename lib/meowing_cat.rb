f5f2348edaa40b26254994d815f5c7127b08c815
Steven Perfors <stevecanbuild@gmail.com>
2020-03-02 19:58:58 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,15 @@
-## code your solution here. 
+class Cat 
+  
+  def name=(cat_name)
+    @name = cat_name
+  end
+  
+  def name 
+    @name 
+  end
+  
+  def meow 
+    puts "meow!"
+  end
+  
+end
